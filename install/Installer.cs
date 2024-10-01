52eb1e3cf234e81b1a02ee9588a4aa19afc8bf0d
SergeyNefyodov <nefyodov.sergei@gmail.com>
2024-10-01 14:50:04 +0200

    Initial commit

---
+++ install/Installer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Installer;
+using WixSharp;
+using WixSharp.CommonTasks;
+using WixSharp.Controls;
+using Assembly = System.Reflection.Assembly;
+
+const string outputName = "SchemaMigrator";
+const string projectName = "SchemaMigrator";
+
+var project = new Project
+{
+    OutDir = "output",
+    Name = projectName,
+    Platform = Platform.x64,
+    UI = WUI.WixUI_FeatureTree,
+    MajorUpgrade = MajorUpgrade.Default,
+    GUID = new Guid("94B17687-E780-43C4-9577-8C2BFDB387DA"),
+    BannerImage = @"install\Resources\Icons\BannerImage.png",
+    BackgroundImage = @"install\Resources\Icons\BackgroundImage.png",
+    Version = Assembly.GetExecutingAssembly().GetName().Version.ClearRevision(),
+    ControlPanelInfo =
+    {
+        Manufacturer = Environment.UserName,
+        ProductIcon = @"install\Resources\Icons\ShellIcon.ico"
+    }
+};
+
+var wixEntities = Generator.GenerateWixEntities(args);
+project.RemoveDialogsBetween(NativeDialogs.WelcomeDlg, NativeDialogs.CustomizeDlg);
+
+BuildSingleUserMsi();
+BuildMultiUserUserMsi();
+
+void BuildSingleUserMsi()
+{
+    project.InstallScope = InstallScope.perUser;
+    project.OutFileName = $"{outputName}-{project.Version}-SingleUser";
+    project.Dirs =
+    [
+        new InstallDir(@"%AppDataFolder%\Autodesk\Revit\Addins\", wixEntities)
+    ];
+    project.BuildMsi();
+}
+
+void BuildMultiUserUserMsi()
+{
+    project.InstallScope = InstallScope.perMachine;
+    project.OutFileName = $"{outputName}-{project.Version}-MultiUser";
+    project.Dirs =
+    [
+        new InstallDir(@"%CommonAppDataFolder%\Autodesk\Revit\Addins\", wixEntities)
+    ];
+    project.BuildMsi();
+}
