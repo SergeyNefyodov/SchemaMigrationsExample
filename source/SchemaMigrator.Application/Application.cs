07aea27a17e13f466189542e276cf9e1a8907cc4
SergeyNefyodov <nefyodov.sergei@gmail.com>
2024-10-01 14:51:58 +0200

    Add application project

---
+++ source/SchemaMigrator.Application/Application.cs
@@ -0,0 +1,31 @@
+﻿using Nice3point.Revit.Toolkit.External;
+using SchemaMigrator.Application.Commands;
+
+namespace SchemaMigrator.Application;
+
+/// <summary>
+///     Application entry point
+/// </summary>
+[UsedImplicitly]
+public class Application : ExternalApplication
+{
+    public override void OnStartup()
+    {
+        Host.Start();
+        CreateRibbon();
+    }
+
+    public override void OnShutdown()
+    {
+        Host.Stop();
+    }
+
+    private void CreateRibbon()
+    {
+        var panel = Application.CreatePanel("Commands", "SchemaMigrator.Application");
+
+        panel.AddPushButton<StartupCommand>("Execute")
+            .SetImage("/SchemaMigrator.Application;component/Resources/Icons/RibbonIcon16.png")
+            .SetLargeImage("/SchemaMigrator.Application;component/Resources/Icons/RibbonIcon32.png");
+    }
+}
